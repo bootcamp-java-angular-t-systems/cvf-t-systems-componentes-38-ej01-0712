8a8abd5be021d753290c66b26d371a389243015f
carlavelasco15 <carlavelasco15@gmail.com>
2023-12-07 17:36:04 +0100

    tests

---
+++ src/app/test/test.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-test',
+  standalone: true,
+  imports: [],
+  templateUrl: './test.component.html',
+  styleUrl: './test.component.css'
+})
+export class TestComponent {
+
+  vars = [
+    { nom: "carla", cognom: 'velasco' },
+    { nom: "marc",  cognom: 'peliccer' },
+
+  ]
+
+}
